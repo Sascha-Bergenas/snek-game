fd4ea92f23df2b24c97944086d27c0abcc5c3fad
Sascha Bergenås <sascha.bergenas@hotmail.com>
2025-12-18 09:12:21 +0100

    board logic

---
+++ board.js
@@ -0,0 +1,49 @@
+export class Board {
+  constructor(columns, rows, cellSize, context) {
+    this.columns = columns;
+    this.rows = rows;
+    this.cellSize = cellSize;
+    this.context = context;
+  }
+
+  drawBackground() {
+    this.context.fillStyle = "#111";
+    this.context.fillRect(
+      0,
+      0,
+      this.columns * this.cellSize,
+      this.rows * this.cellSize
+    );
+  }
+
+  drawGrid() {
+    this.context.strokeStyle = "rgba(255, 255, 255, 0.2)";
+
+    // lodräta linjer
+    for (let column = 0; column <= this.columns; column++) {
+      const x = column * this.cellSize;
+      this.context.beginPath();
+      this.context.moveTo(x, 0);
+      this.context.lineTo(x, this.rows * this.cellSize);
+      this.context.stroke();
+    }
+
+    // vågräta linjer
+    for (let row = 0; row <= this.rows; row++) {
+      const y = row * this.cellSize;
+      this.context.beginPath();
+      this.context.moveTo(0, y);
+      this.context.lineTo(this.columns * this.cellSize, y);
+      this.context.stroke();
+    }
+  }
+  drawCell(column, row, color = "lime") {
+    this.context.fillStyle = color;
+    this.context.fillRect(
+      column * this.cellSize,
+      row * this.cellSize,
+      this.cellSize,
+      this.cellSize
+    );
+  }
+}
