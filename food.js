4aefbd7d7a9a90e8e494f13a14025c3bb0a22f88
Sascha Bergenås <sascha.bergenas@hotmail.com>
2025-12-19 11:32:28 +0100

    food modul

---
+++ food.js
@@ -0,0 +1,21 @@
+export function createFood(columns, rows, snake) {
+  let food;
+
+  do {
+    food = {
+      x: Math.floor(Math.random() * columns),
+      y: Math.floor(Math.random() * rows),
+    };
+  } while (isOnSnake(food, snake));
+
+  return food;
+}
+
+function isOnSnake(food, snake) {
+  for (let part of snake.snake) {
+    if (part.x === food.x && part.y === food.y) {
+      return true;
+    }
+  }
+  return false;
+}
